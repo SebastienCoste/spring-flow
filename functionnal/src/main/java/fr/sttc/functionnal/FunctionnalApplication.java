10818a47ef73428be01707880ad22435629c4102
Sebastien Coste <teddy.coste@gmail.com>
2017-11-28 08:55:13 +0100

    rework

---
+++ functionnal/src/main/java/fr/sttc/functionnal/FunctionnalApplication.java
@@ -0,0 +1,12 @@
+package fr.sttc.functionnal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunctionnalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunctionnalApplication.class, args);
+	}
+}
